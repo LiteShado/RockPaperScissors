1f099c493de38cb270c3d7dde6a9140d27192b78
Nat Richmond <natrichmond@Nats-MBP.lan>
2025-01-11 23:45:08 -0500

    showResults function updated to allow for game reset functionality

--- script.js
+++ script.js
@@ -36,10 +36,25 @@ function getRandomComputerResult() {
 const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
 
 function showResults(userOption) {
   roundResultsMsg.innerText = getRoundResults(userOption);
-  playerScoreSpanElement.innerText = playerScore;
   computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+  if (computerScore === 3) {
+    winnerMsgElement.innerText = "Computer has won the game!";
+    optionsContainer.style.display = "none";
+    resetGameBtn.style.display = "initial";
+
+  } else if (playerScore === 3) {
+    winnerMsgElement.innerText = "Player has won the game!";
+    optionsContainer.style.display = "none";
+    resetGameBtn.style.display = "initial";
+
+  }
 
 };
